3cd6ae0a4207a6efd0f70db185296fc8ca3f55b1
baweuh <110043953+baweuh@users.noreply.github.com>
2022-09-26 11:14:11 +0200

    Update IA-Immo.py

--- IA-Immo.py
+++ IA-Immo.py
@@ -2,19 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-# Creation of a sample of 200 values
+# Création d'un échantillon de 200 valeurs
 m = 200
 x = np.array([740,880,650,2630,1895])
 y = np.array([25,35,19,103,67])
 
-# Creation of the regression model
+# Création d'un modèle de régression
 model = LinearRegression()
-# Training of the model
+# Entraînement du modèle
 model.fit(x.reshape(-1, 1), y)
 
 print("Le score du modèle est de :", model.score(x.reshape(-1, 1), y))
 print("Avec 1000€ vous pouvez prétendre à un appartement de", model.predict(np.array(1000).reshape(-1, 1)), "m²")
 
+# Modélisation d'un graphique à l'aide des deux tableaux
 plt.scatter(x, y)
 plt.plot(x.reshape(-1, 1), model.predict(x.reshape(-1, 1)), c = 'red')
 plt.title("Prix d'un loyer en fonction d'une surface")
